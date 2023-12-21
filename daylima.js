b88d7d1cb2785d572731c4b38a988e7aa412fda9
sm-rijal <srijal@binaracademy.org>
2023-12-21 21:54:39 +0700

    pertemuan 5

---
+++ daylima.js
@@ -0,0 +1,89 @@
+// git
+// latihan git
+
+
+
+
+
+
+//1. challenge git
+// buatlah repository untuk menyimpan code yang ada di local
+
+// 2. challenge variable manipulasi string
+// buat file namafile challenge-variable.js atau variable.js
+// tampilkan data menggunakan ``
+// let namaDepan = "Samsul"
+// let namaBelakang = "Rijal"
+// `halo nama saya variable1 variable`
+// let data = "Halo nama saya Adit saya sedang belajar javascript"
+// ganti saya jadi aku
+// ouput = Hallo nama aku Adit aku sedang belajar javascript
+
+
+// 3.challenge array object
+// 4.challenge function
+
+
+
+// array 
+// let nama1 = "Adit"
+// let nama2 = "Arinal"
+// let nama3 = "Mariana"
+// console.log(nama1);
+// console.log(nama2);
+// console.log(nama3);
+let namaOrang = ["Adit","Arinal","Mariana"]
+console.log(namaOrang);
+
+// object
+let dataAdit = {
+    nama: "Adit", 
+    hobi: "berenang"
+}
+let dataArinal = {
+    nama: "Arinal", 
+    hobi: "olahraga"
+}
+
+// array object
+let dataOrang = [
+    {
+        nama: "Adit", 
+        hobi: "berenang"
+    },
+    {
+        nama: "Arinal", 
+        hobi: "olahraga"
+    }
+]
+
+console.log(dataOrang);
+
+
+let kursus = [
+    {
+        gambar: "gambar.jpg",
+        judul: "kursus1",
+        deskrpsi: "deskripsi kursus",
+        harga: 20000
+    },
+    {
+        gambar: "gambar2.jpg",
+        judul: "kursus2",
+        deskrpsi: "deskripsi kursus2",
+        harga: 15000
+    },
+]
+
+console.log(kursus);
+
+// jelaskan materi code
+//1. variable -> const, let, var
+// let data = "Rijal"
+//2. tipe data string, number, bool
+//3. manipulasi string, contoh replace, subtsring
+//4 kondisi menggunakan if else dan tenary operator ? :
+//5. data object
+//6. data array
+//7. data array object
+//8. function punya parameter
